e873c51c311a3c05ca8b26f92c157fdc1924bf48
Aaron R Miller <amiller@watheia.org>
2021-06-11 05:56:39 +0000

    use yarn v1

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,5 @@
-import Alert from "./alert"
-import Footer from "./footer"
+import Alert from "@components/alert"
+import Footer from "@components/footer"
 
 export default function Layout({ preview, children }) {
   return (
